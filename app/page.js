5bb1bfba5cb6391f5f58009b3ab2040a927cd24d
Zacchaeus Napuo <zacchaeus.napuo@amalitech.org>
2023-03-25 11:02:38 +0000

    tailwindcss config

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main className="">
-      
+      <h1 className="text-3xl text-[red] font-bold underline">Hello global world!</h1>
     </main>
-  )
+  );
 }
