3c190b5c16e099aab73b8f181ebccbcd4164f987
Zacchaeus Napuo <zacchaeus.napuo@amalitech.org>
2023-03-25 12:07:09 +0000

    add header section

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main className="">
-      <h1 className="text-3xl text-[red] font-bold underline">Hello global world!</h1>
+      
     </main>
   );
 }
---
+++ components/header/Header.jsx
@@ -0,0 +1,29 @@
+"use client";
+import React from "react";
+import { AiFillHome } from "react-icons/ai";
+import { BsFillInfoCircleFill } from "react-icons/bs";
+import Link from "next/link";
+import MenuItem from "../menuItem/MenuItem";
+
+const Header = () => {
+  return (
+    <div className="flex justify-between max-w-6xl sm:mx-auto items-center mx-4 py-6">
+      {/* menu */}
+      <div className="flex space-x-4">
+        <MenuItem title="HOME" address="/" Icon={AiFillHome} />
+        <MenuItem title="ABOUT" address="/about" Icon={BsFillInfoCircleFill} />
+      </div>
+      {/* logo */}
+      <div className="">
+        <Link href="/">
+          <h2 className="text-2xl">
+            <span className="font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1">IMDb</span>
+            <span className="">Clone</span>
+          </h2>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
