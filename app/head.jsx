3e9fc3b29a7cb43522afa5af6abd3ed71791a0b5
Zacchaeus Napuo <zacchaeus.napuo@amalitech.org>
2023-03-25 10:39:46 +0000

    initial cleanup

---
+++ app/head.jsx
@@ -0,0 +1,11 @@
+export default function Head() {
+  return (
+    <>
+      <title>IMDB Clone</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" name="description" content="This is IMDB Clone" />
+      <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+    </>
+  );
+}
