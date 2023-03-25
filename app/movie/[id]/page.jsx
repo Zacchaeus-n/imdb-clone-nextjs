3e4ab5724c33b3991bade9eff5ee5785e0fb35f1
Zacchaeus Napuo <zacchaeus.napuo@amalitech.org>
2023-03-25 15:26:23 +0000

    data checks

--- app/movie/[id]/page.jsx
+++ app/movie/[id]/page.jsx
@@ -15,7 +15,7 @@ export default async function MoviePage({ params }) {
       <div className="p-4 md:pt-8 flex flex-col md:flex-row items-center content-center max-w-6xl mx-auto md:space-x-6">
         <Image
           src={`https://image.tmdb.org/t/p/original/${
-            movie.backdrop_path || movie.poster_path
+            movie?.backdrop_path || movie?.poster_path
           }`}
           width={500}
           height={300}
@@ -26,23 +26,23 @@ export default async function MoviePage({ params }) {
           }}
           placeholder="blur"
           blurDataURL="/spinner.svg"
-          alt="Movie poster"
+          alt="Movie? poster"
         ></Image>
         <div className="p-2">
           <h2 className="text-lg mb-3 font-bold">
-            {movie.title || movie.name}
+            {movie?.title || movie?.name}
           </h2>
           <p className="text-lg mb-3">
             <span className="font-semibold mr-1">Overview:</span>
-            {movie.overview}
+            {movie?.overview}
           </p>
           <p className="mb-3">
             <span className="font-semibold mr-1">Date Released:</span>
-            {movie.release_date || movie.first_air_date}
+            {movie?.release_date || movie?.first_air_date}
           </p>
           <p className="mb-3">
             <span className="font-semibold mr-1">Rating:</span>
-            {movie.vote_count}
+            {movie?.vote_count}
           </p>
         </div>
       </div>
